f4b6ce2af58a8b7ed515a47dbead8161ce94df47
glu000 <gl@gl.co.at>
2020-01-03 08:51:56 +0100

    Update renderer.php

--- course/format/grid/renderer.php
+++ course/format/grid/renderer.php
@@ -519,7 +519,8 @@ class format_grid_renderer extends format_section_renderer_base {
                 $PAGE->requires->js_init_call('M.format_grid.init', array(
                     $PAGE->user_is_editing(),
                     $sectionredirect,
-                    $coursenumsections,
+                    count($this->shadeboxshownarray),
+                    $this->initialsection,
                     json_encode($this->shadeboxshownarray)));
                 if (!$PAGE->user_is_editing()) {
                     // Initialise the key control functionality...
